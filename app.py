afc9690105a073ef26970162c43e9275f81f1205
Jose <jocvegar@gmail.com>
2020-05-09 11:37:46 -0600

    updates wording

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
+from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    now = datetime.utcnow().year
+    name = 'Jose V.'
+    return render_template('home.html', now = now, name = name)
 
 if __name__ == '__main__':
     app.run(debug=True)
